55e104efd922cc63c0b72453f3b2a482f18756ff
Gandalf- <austin.voecks@gmail.com>
2014-11-08 11:43:22 -0800

    Added Help-Mode
    Click on the help-mode button, and then any other button to get a short
    description of what it does

--- LockNut.rkt
+++ LockNut.rkt
@@ -17,6 +17,9 @@
                   10
                   'modern))
 
+(define help-mode #f)
+(define wait-time 1.5)
+
 ;MAIN-WINDOW
 ;---------------------------------------------
 (define main-frame 
@@ -35,10 +38,14 @@
                 (stretchable-height #f)
                 ))
 
-;Panel for options and info
+;Panel for options
 (define lower-panel (instantiate horizontal-panel% (main-frame)
                       (stretchable-height #f)
                       ))
+
+(define lowest-panel (instantiate horizontal-panel% (main-frame)
+                       (stretchable-height #f)
+                       ))
 
 ;Gets optional password from user
 ; Default is to generate a seed that alters the encryption algorithm's output
@@ -57,13 +64,24 @@
        (parent panel)
        (horiz-margin 17)
        (callback (lambda (button event)
-                   ;Run encrypt/decrypt, update status text
-                   (send main-frame set-status-text 
-                         (run-encrypt-decrypt (send passcode-field get-value)
-                                              (send password-is-key-checkbox get-value)))
-                   ;Push password changes into other passcode fields
-                   (send create-file-passcode-field set-value (send passcode-field get-value))
+                   (if (equal? help-mode #f)
+                       (begin
+                         ;Run encrypt/decrypt, update status text
+                         (send main-frame set-status-text 
+                               (run-encrypt-decrypt (send passcode-field get-value)
+                                                    (send password-is-key-checkbox get-value)))
+                         ;Push password changes into other passcode fields
+                         (send create-file-passcode-field set-value (send passcode-field get-value)))
+                       ;Help mode
+                       (begin
+                         (send main-frame set-status-text "Choose a text file to be encrypted...")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text " or a .locknut file to be unencrypted")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text " ")
+                         (set! help-mode #f)))
                    ))
+       
        ))
 
 ;Button for glance procedure
@@ -73,15 +91,27 @@
        (parent panel)
        (horiz-margin 8)
        (callback (lambda (button event)
-                   ;Run glance, update status text
-                   (send main-frame set-status-text
-                         (glance (send passcode-field get-value)
-                                 (send password-is-key-checkbox get-value)))
-                   ;Push password changes into other passcode fields
-                   (send create-file-passcode-field set-value (send passcode-field get-value))
+                   (if (equal? help-mode #f)
+                       (begin
+                         ;Run glance, update status text
+                         (send main-frame set-status-text
+                               (glance (send passcode-field get-value)
+                                       (send password-is-key-checkbox get-value)))
+                         ;Push password changes into other passcode fields
+                         (send create-file-passcode-field set-value (send passcode-field get-value)))
+                       ;Help mode
+                       (begin
+                         (set! help-mode #f)
+                         (send main-frame set-status-text "Choose a .locknut file to view unecrypted...")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text " but keep the file itself encrypted.")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text " ")))
                    ))
        ))
 
+;CREATE FILE
+;----------------------------------------------------------------------
 (define create-file-frame
   (new frame%
        (label "New Encrypted File")
@@ -91,14 +121,24 @@
 
 (send create-file-frame create-status-line)
 
-;Button that opens create-file-frame
+;Button that opens create-file-frame, MAIN-FRAME
 (define create-file-button
   (new button%
-       (label "Create File")
+       (label "Create New File")
        (parent lower-panel)
-       (horiz-margin 8)
+       (horiz-margin 30)
        (callback (lambda (button event)
-                   (send create-file-frame show #t)))
+                   (if (equal? help-mode #f)
+                       (send create-file-frame show #t)
+                       ;Help mode
+                       (begin
+                         (set! help-mode #f)
+                         (send main-frame set-status-text "Opens window with options for creating a...")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text "new encrypted file right now")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text " ")))
+                   ))
        ))
 
 ;Panel for create-file buttons
@@ -149,11 +189,28 @@
 ;Message explaining details
 (define create-file-message
   (new message%
-       (label "\nFilename: name of the new encrypted file \nPassword: optional password used during encryption \n Create : Creates, opens file. Make your edits\n and save, then close notepad. File is now encrypted.")
+       (label "Filename: name of the new encrypted file \nPassword: optional password used during encryption \n Create : Creates, opens file. Make your edits\n and save, then close notepad. File is now encrypted.")
        (font my-font)
        (parent create-file-frame)
        ))
 
+;Button that opens advanced options window
+(define options-button
+  (new button%
+       (label "Options")
+       (parent lower-panel)
+       (horiz-margin 8)
+       (callback (lambda (button event)
+                   (if (equal? help-mode #f)
+                   (send options-frame show #t)
+                   ;Help mode
+                   (begin
+                     (set help-mode #f)
+                     (send main-frame set-status-text "Opens window with advanced options")
+                     (sleep/yield wait-time)
+                     (send main-frame set-status-text " ")))
+                   ))
+       ))
 
 ;OPTIONS WINDOW
 ;-----------------------------------------------
@@ -165,16 +222,6 @@
        ))
 
 (send options-frame create-status-line)
-
-;Button that opens advanced options window
-(define options-button
-  (new button%
-       (label "Options")
-       (parent lower-panel)
-       (horiz-margin 8)
-       (callback (lambda (button event)
-                   (send options-frame show #t)))
-       ))
 
 ;Panel for options buttons
 (define options-panel (instantiate vertical-panel% (options-frame)
@@ -194,29 +241,50 @@
                        (send options-frame set-status-text "Encryption key set back to default values"))))
        ))
 
+;Button that displays info, MAIN-FRAME
+(define info-button
+  (new button%
+       (label "Information")
+       (parent lowest-panel)
+       (horiz-margin 40)
+       (callback (lambda (button event)
+                   (if (equal? help-mode #f)
+                       (send info-frame show #t)
+                       ;Help mode
+                       (begin
+                         (send main-frame set-status-text "Open window with program information")
+                         (sleep/yield wait-time)
+                         (send main-frame set-status-text " ")
+                         (set! help-mode #f)))
+                   ))
+       ))
+
+;Help button, MAIN-FRAME
+(define help-button
+  (new button%
+       (label "Help Mode")
+       (parent lowest-panel)
+       (horiz-margin 3)
+       (callback (lambda
+                     (button event)
+                   (set! help-mode #t)
+                   (send main-frame set-status-text "Click on a button for help")))
+       ))
 
 ;INFORMATION WINDOW
 ;-----------------------------------------------
 (define info-frame
   (new frame%
        (label "Information")
-       (min-width 200)
-       (min-height 100)
-       ))
-
-;Button that displays info
-(define info-button
-  (new button%
-       (label "Info")
-       (parent lower-panel)
-       (horiz-margin 8)
-       (callback (lambda (button event)
-                   (send info-frame show #t)))
+       (width 200)
+       (height 100)
+       (stretchable-width #f)
+       (stretchable-height #f)
        ))
 
 (define info-message
   (new message%
-       (label "LockNut: personal file encryption program\n\n Protect personal files with a powerful Vigenere \n cipher using password verification.\n\n  Austin Voecks, Summer 2014\n")
+       (label "LockNut: personal file encryption program  \n Austin Voecks, Summer 2014")
        (font my-font)
        (parent info-frame)
        ))
